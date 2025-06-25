0631b9356a83d43c5b01454f49ff12a45b89ba4b
Estanler Aleman <Esencymusic@gmail.com>
2025-06-25 03:46:07 -0400

    Add exercises for Variables, Data Types, and Operators

--- index.js
+++ index.js
@@ -24,3 +24,38 @@ console.log('Hello, World!');
 
 // TODO: Uncomment the line below and run the code
 // log('Hello, World!');
+
+// Exercise 2: Variables and Data Types
+let name = "Estanler";
+const age = 35;
+var isStudent = true;
+let greeting = "Hello";
+let score = 99;
+let passed = true; 
+let emptyvalue = null;
+let unkownvalue;
+let user = { name: "Estanler", age: 35 };
+ 
+console.log(typeof greeting); // string
+console.log(typeof score); // number
+console.log(typeof passed); // boolean
+console.log(typeof emptyvalue); // object (null is a special case in JavaScript)
+console.log(typeof unkownvalue); // undefined
+console.log(typeof user); // object
+
+//Exercise 3: Operators
+let x = 10;
+let y = 3;
+
+console.log(x + y); // Addition
+console.log(x - y); // Subtraction
+console.log(x * y); // Multiplication
+console.log(x / y); // Division
+console.log("Remainder", x % y); // Modulus
+
+console.log("Is x equal to y?", x == y); 
+console.log("Is x stricly equal to y?", x === y);
+console.log("Is x not equal to y?", x != y);
+console.log("Is x greater than y?", x > y);
+console.log("Is x less than or equal to y?", x <= y);
+
